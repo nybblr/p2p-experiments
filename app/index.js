21978dd011cdf7a8ab2ce6e7aecbb9cbccbf47cb
Jonathan Martin <me@nybblr.com>
2016-05-27 17:04:18 -0500

    Use Gulf OTs to sync peers! *dance*

--- app/index.js
+++ app/index.js
@@ -1,6 +1,30 @@
 var peer = require('./peer');
 
+var textOT = require('ottypes').text
+var gulf = require('gulf');
+var bindEditor = require('gulf-textarea');
+var textarea = document.querySelector('textarea#doc');
+var textareaDoc = bindEditor(textarea);
+var text = 'hello';
+
 peer('chat.sock').then(stream => {
   console.log('started');
+  console.log(stream.server ? 'master' : 'slave');
   window.stream = stream;
+
+  var textareaMaster = textareaDoc.masterLink();
+
+  if (stream.server) {
+    // master
+    gulf.Document.create(new gulf.MemoryAdapter, textOT, text, (err, doc) => {
+      var slave1 = doc.slaveLink();
+      stream.pipe(slave1).pipe(stream);
+
+      var slave2 = doc.slaveLink();
+      textareaMaster.pipe(slave2).pipe(textareaMaster);
+    });
+  } else {
+    // slave
+    textareaMaster.pipe(stream).pipe(textareaMaster);
+  }
 });
