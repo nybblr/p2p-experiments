abecce6c14b1c05a4331a1e998b1d0bb12704570
Jonathan Martin <me@nybblr.com>
2016-07-28 09:50:33 -0500

    WIP Glen editor integration.

--- app/resolvers/glen.js
+++ app/resolvers/glen.js
@@ -7,26 +7,63 @@ var crate = require('crate-core');
 
 module.exports = stream => {
   var doc = new crate(null, stream);
+  window.doc = doc;
 
+  if (stream.server) {
+    console.log('Sending initial state');
+
+    // Sssshhhh
+    var send = stream.send;
+    stream.send = () => {};
+
+    // Start off doc with contents of textarea.
+    doc.insert('\n', 0);
+
+    stream.send = send;
+
+    var initial = JSON.stringify(doc.sequence);
+    doc.broadcast.send(initial);
+    edit(doc);
+  } else {
+    console.log('Awaiting initial state...');
+    var cb = (initial) => {
+      console.log('Received initial state...');
+      doc.sequence.fromJSON(JSON.parse(initial));
+      edit(doc);
+    };
+    doc.broadcast.once('message', cb);
+  }
+}
+
+function getStringChildNode(childNode){
+  var result = '';
+  if (childNode.e !== null){ result = childNode.e; };
+  for (var i=0; i<childNode.children.length; ++i){
+    result += getStringChildNode(childNode.children[i]);
+  };
+  return result;
+};
+
+var docToString = (doc) => getStringChildNode(doc.sequence.root);
+
+var isConsistent = (doc, editor) => docToString(doc) === editor.getContent();
+
+var edit = (doc) => {
   var editor = window.editor;
 
-  // #B initialize the string within the editor
-  function getStringChildNode(childNode){
-    var result = '';
-    if (childNode.e !== null){ result = childNode.e; };
-    for (var i=0; i<childNode.children.length; ++i){
-      result += getStringChildNode(childNode.children[i]);
-    };
-    return result;
+  editor.setContent(docToString(doc));
+
+  var debug = () => {
+    console.log(docToString(doc));
+
+    if(!isConsistent(doc, editor)) {
+      console.warn('INCONSISTENT');
+      debugger;
+    }
   };
 
-  // Start off doc with contents of textarea.
-  // doc.insert('\n', 0);
-
-  editor.setContent(getStringChildNode(doc.sequence.root));
-
-  var insertRemoveOp = false;
-
+  // var insertRemoveOp = false;
+  //
   // editor.getSession().getSelection().on('changeCursor', function(e, sel){
   //   if (!insertRemoveOp){
   //     var range = sel.getRange();
@@ -93,46 +130,41 @@ module.exports = stream => {
 
     var begin, end, text, message, j=0;
 
-    console.log('Change:');
-
     begin = e.start;
     end = e.end;
-
-    console.log(e);
-    console.log(`from ${begin} to ${end}`);
 
     for (var i=begin; i<end; ++i){
       switch (e.action){
         case 'insert':
-          console.log('insert: ' + text[j]);
+          console.log('insert: ' + text[j] + ' at ' + i);
           doc.insert(text[j], i);
-          console.log(`${text[j]}, ${i}`);
           break;
         case 'remove':
-          console.log('remove');
+          console.log('remove: ' + begin);
           doc.remove(begin);
-          console.log(begin)
-            break;
+          break;
       };
       ++j;
     };
+
+    debug();
   });
 
   doc.on('remoteInsert', (element, index) => {
     if (index === -1) { return; }
     editor.watcherExt.noWatch(() => {
-      // editor.watcher.noWatch(() => {
-        editor.replace(index, index, element);
-      // });
+      console.log('remote insert: ' + element + ' at ' + index);
+      editor.replace(index-1, index-1, element);
     });
+    debug();
   });
 
   doc.on('remoteRemove', (index) => {
     if (index === -1) { return; }
     editor.watcherExt.noWatch(() => {
-      // editor.watcher.noWatch(() => {
-        editor.replace(index, index+1, '');
-      // });
+      console.log('remote remove: ' + index);
+      editor.replace(index-1, index, '');
     });
+    debug();
   });
 };
